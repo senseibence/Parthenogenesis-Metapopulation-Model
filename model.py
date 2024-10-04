2894e95588f0d0fc9a899cc7ac3e58db49eec18e
Bence Lukacsy <bence.lukacsy@gmail.com>
2024-10-03 20:15:13 -0400

    adjusted code to match parth19bas

--- model.py
+++ model.py
@@ -2,20 +2,20 @@
 # parth 19 adds option to have no migration, i.e. no subpopulation
 # get run parameters from user
 
-PopSize=100
-# PopSize = int(input("Enter size of metapopulation: "))
+popsize=100
+# popsize = int(input("Enter size of metapopulation: "))
 
-MaxSubSize=50
-# MaxSubSize = int(input("Enter maximum size of subpopulation: "))
+maxsubsize=50
+# maxsubsize = int(input("Enter maximum size of subpopulation: "))
 
-migRecip=1000
-# migRecip = int(input("Enter reciprocal of migration rate [enter 0 for no migration; i.e. no subpopulation]: "))
-Migration = 0
-if (migRecip != 0): Migration = 1/migRecip
+migrecip=1000
+# migrecip = int(input("Enter reciprocal of migration rate [enter 0 for no migration; i.e. no subpopulation]: "))
+migration = 0
+if (migrecip != 0): migration = 1/migrecip
 
-mutRecip=1000
-# mutRecip = int(input("Enter reciprocal of mutation rate: "))
-Mutation = 1/mutRecip
+mutrecip=1000
+# mutrecip = int(input("Enter reciprocal of mutation rate: "))
+mutation = 1/mutrecip
 
 '''
 rem ? "enter recombination rate between Locus 1 and Locus 2"
@@ -24,29 +24,29 @@ rem ? "enter recombination rate between Locus 2 and Locus 3”
 rem input Rec2
 '''
 
-Rec1 = 0.5
-Rec2 = 0.5
+rec1 = 0.5
+rec2 = 0.5
 
-NumInds=4
-# NumInds = int(input("Enter number of individuals encountered in main population: "))
+numinds=4
+# numinds = int(input("Enter number of individuals encountered in main population: "))
 
-NumIndsSub=10
-# NumIndsSub = int(input("Enter number of individuals encountered in sub population: "))
+numindssub=10
+# numindssub = int(input("Enter number of individuals encountered in sub population: "))
 
-MaxRepro=10
-# MaxRepro = int(input("Enter number of offspring per female: "))
+maxrepro=10
+# maxrepro = int(input("Enter number of offspring per female: "))
 
 f=1
 # f = int(input("Choose fitness dominance [1] or fitness overdominance [2]: "))
 
-OverDom = 0
-if (f != 1): OverDom = 1
+overdom = 0
+if (f != 1): overdom = 1
 
-NumGens=5
-# NumGens = int(input("Enter number of generations in the run: "))
+numgens=5
+# numgens = int(input("Enter number of generations in the run: "))
 
-Outfile="test.txt"
-# Outfile = input("Enter name of output file: ")
+outfile="test.txt"
+# outfile = input("Enter name of output file: ")
 
 '''
 rem ? "enter random seed"
@@ -54,113 +54,110 @@ rem input RandomSeed
 '''
 
 import random
-RandomSeed = random.randint(0, 4999)
-random.seed(RandomSeed)
+randomseed = random.randint(0, 4999)
+random.seed(randomseed)
 
-ParthReduction=0.9
-# ParthReduction = float(input("Enter % reproduction of Parthenogenesis vs. sexual reproduction: "))
+parthreduction=0.9
+# parthreduction = float(input("Enter % reproduction of Parthenogenesis vs. sexual reproduction: "))
 
-ParthRepro = int(ParthReduction*MaxRepro+0.5)
+parthrepro = int(parthreduction*maxrepro+0.5)
 
-print("Sexual reproduction produces "+str(MaxRepro)+" offspring. Parthenogenetic reproduction produces "+str(ParthRepro)+" offspring.")
+print("Sexual reproduction produces "+str(maxrepro)+" offspring. Parthenogenetic reproduction produces "+str(parthrepro)+" offspring.")
 
 x = 0.9
 # x = float(input("Enter % reproduction for parthenogenetic-capable females reproducing sexually: "))
 
-ParthPenalty = round(1-x, 3)
+parthpenalty = round(1-x, 3)
 
 # input("Hit any key followed by <return> to continue: ")
 
 # print all the variables
-print(f"PopSize: {PopSize}")
-print(f"MaxSubSize: {MaxSubSize}")
-print(f"migRecip: {migRecip}")
-print(f"Migration: {Migration}")
-print(f"mutRecip: {mutRecip}")
-print(f"Mutation: {Mutation}")
-print(f"Rec1: {Rec1}")
-print(f"Rec2: {Rec2}")
-print(f"NumInds: {NumInds}")
-print(f"NumIndsSub: {NumIndsSub}")
-print(f"MaxRepro: {MaxRepro}")
+print(f"PopSize: {popsize}")
+print(f"MaxSubSize: {maxsubsize}")
+print(f"migRecip: {migrecip}")
+print(f"Migration: {migration}")
+print(f"mutRecip: {mutrecip}")
+print(f"Mutation: {mutation}")
+print(f"Rec1: {rec1}")
+print(f"Rec2: {rec2}")
+print(f"NumInds: {numinds}")
+print(f"NumIndsSub: {numindssub}")
+print(f"MaxRepro: {maxrepro}")
 print(f"f: {f}")
-print(f"OverDom: {OverDom}")
-print(f"NumGens: {NumGens}")
-print(f"Outfile: {Outfile}")
-print(f"RandomSeed: {RandomSeed}")
-print(f"ParthReduction: {ParthReduction}")
-print(f"ParthRepro: {ParthRepro}")
-print(f"ParthPenalty: {ParthPenalty}")
+print(f"OverDom: {overdom}")
+print(f"NumGens: {numgens}")
+print(f"Outfile: {outfile}")
+print(f"RandomSeed: {randomseed}")
+print(f"ParthReduction: {parthreduction}")
+print(f"ParthRepro: {parthrepro}")
+print(f"ParthPenalty: {parthpenalty}")
 
 # helper function to create matrix
 def createMatrix(rows, cols):
     return [[0 for x in range(cols)] for y in range(rows)]
 
 # dimensioning
-Sex = [0] * (PopSize*2 + 1)
-Location = [0] * (PopSize + 1)
-Loc1Allele1 = [0] * (PopSize + 1)
-Loc1Allele2 = [0] * (PopSize + 1)
-Loc2Allele1 = [0] * (PopSize + 1)
-Loc2Allele2 = [0] * (PopSize + 1)
-Loc3Allele1 = [0] * (PopSize + 1)
-Loc3Allele2 = [0] * (PopSize + 1)
-OffspringAlive = [0] * (MaxRepro*PopSize + 1)
-OffspringSex = [0] * (MaxRepro*PopSize + 1)
-OffspringLocation = [0] * (MaxRepro*PopSize + 1)
-OffspringLoc1Allele1 = [0] * (MaxRepro*PopSize + 1)
-OffspringLoc1Allele2 = [0] * (MaxRepro*PopSize + 1)
-OffspringLoc2Allele1 = [0] * (MaxRepro*PopSize + 1)
-OffspringLoc2Allele2 = [0] * (MaxRepro*PopSize + 1)
-OffspringLoc3Allele1 = [0] * (MaxRepro*PopSize + 1)
-OffspringLoc3Allele2 = [0] * (MaxRepro*PopSize + 1)
-PopInds = [0] * (PopSize + 1)
-SubInds = [0] * (PopSize + 1)
-Meeting = createMatrix(PopSize,NumInds)
-SexMothers = [0] * (PopSize + 1)
-SexFathers = [0] * (PopSize + 1)
-ParthMothers = [0] * (PopSize + 1)
-score = [0] * (MaxRepro*PopSize + 1)
-OriginalIndex = [0] * (MaxRepro*PopSize + 1)
-NewOffspringAlive = [0] * (MaxRepro*PopSize + 1)
-NewOffspringSex = [0] * (MaxRepro*PopSize + 1)
-NewOffspringLocation = [0] * (MaxRepro*PopSize + 1)
-NewOffspringLoc1Allele1 = [0] * (MaxRepro*PopSize + 1)
-NewOffspringLoc1Allele2 = [0] * (MaxRepro*PopSize + 1)
-NewOffspringLoc2Allele1 = [0] * (MaxRepro*PopSize + 1)
-NewOffspringLoc2Allele2 = [0] * (MaxRepro*PopSize + 1)
-NewOffspringLoc3Allele1 = [0] * (MaxRepro*PopSize + 1)
-NewOffspringLoc3Allele2 = [0] * (MaxRepro*PopSize + 1)
-Loc1Freq = [0] * (2*NumGens + 1)
-Loc2Freq = [0] * (2*NumGens + 1)
-Loc3Freq = [0] * (2*NumGens + 1)
-MainPopCount = [0] * (2*NumGens + 1)
-SubPopCount = [0] * (2*NumGens + 1)
-SexualOffspring = [0] * (2*NumGens + 1)
-ParthOffspring = [0] * (2*NumGens + 1)
-Sexuals = [0] * (2*NumGens + 1)
-Parths = [0] * (2*NumGens + 1)
-ParthMode = [0] * (2*NumGens + 1)
-frequency = [0] * (2*PopSize + 1)
-ModeFreq = [0] * (2*NumGens + 1)
+sex = [0] * (popsize*2 + 1)
+location = [0] * (popsize + 1)
+loc1allele1 = [0] * (popsize + 1)
+loc1allele2 = [0] * (popsize + 1)
+loc2allele1 = [0] * (popsize + 1)
+loc2allele2 = [0] * (popsize + 1)
+loc3allele1 = [0] * (popsize + 1)
+loc3allele2 = [0] * (popsize + 1)
+offspringalive = [0] * (maxrepro*popsize + 1)
+offspringsex = [0] * (maxrepro*popsize + 1)
+offspringlocation = [0] * (maxrepro*popsize + 1)
+offspringloc1allele1 = [0] * (maxrepro*popsize + 1)
+offspringloc1allele2 = [0] * (maxrepro*popsize + 1)
+offspringloc2allele1 = [0] * (maxrepro*popsize + 1)
+offspringloc2allele2 = [0] * (maxrepro*popsize + 1)
+offspringloc3allele1 = [0] * (maxrepro*popsize + 1)
+offspringloc3allele2 = [0] * (maxrepro*popsize + 1)
+popinds = [0] * (popsize + 1)
+subinds = [0] * (popsize + 1)
+Meeting = createMatrix(popsize,numinds)
+sexmothers = [0] * (popsize + 1)
+sexfathers = [0] * (popsize + 1)
+parthmothers = [0] * (popsize + 1)
+score = [0] * (maxrepro*popsize + 1)
+originalindex = [0] * (maxrepro*popsize + 1)
+newoffspringalive = [0] * (maxrepro*popsize + 1)
+newoffspringsex = [0] * (maxrepro*popsize + 1)
+newoffspringlocation = [0] * (maxrepro*popsize + 1)
+newoffspringloc1allele1 = [0] * (maxrepro*popsize + 1)
+newoffspringloc1allele2 = [0] * (maxrepro*popsize + 1)
+newoffspringloc2allele1 = [0] * (maxrepro*popsize + 1)
+newoffspringloc2allele2 = [0] * (maxrepro*popsize + 1)
+newoffspringloc3allele1 = [0] * (maxrepro*popsize + 1)
+newoffspringloc3allele2 = [0] * (maxrepro*popsize + 1)
+loc1freq = [0] * (2*numgens + 1)
+loc2freq = [0] * (2*numgens + 1)
+loc3freq = [0] * (2*numgens + 1)
+mainpopcount = [0] * (2*numgens + 1)
+subpopcount = [0] * (2*numgens + 1)
+sexualoffspring = [0] * (2*numgens + 1)
+parthoffspring = [0] * (2*numgens + 1)
+sexuals = [0] * (2*numgens + 1)
+parths = [0] * (2*numgens + 1)
 
 # keep track of the maximum value of locus 2 allele that has appeared
-MaxParth = 0
+maxparth = 0
 
 # set initial arrays
-for i in range(1, PopSize+1):
+for i in range(1, popsize+1):
     aa = random.random()
-    if (aa < 0.5): Sex[i] = 1
-    else: Sex[i] = 0
-    Location[i] = 0
-    Loc1Allele1[i] = 1
-    if (OverDom == 1): Loc1Allele2[i] = 0
-    else: Loc1Allele2[i] = 1
-    Loc2Allele1[i] = 0
-    Loc2Allele2[i] = 0
-    Loc3Allele1[i] = 0
-    Loc3Allele2[i] = 0
+    if (aa < 0.5): sex[i] = 1
+    else: sex[i] = 0
+    location[i] = 0
+    loc1allele1[i] = 1
+    if (overdom == 1): loc1allele2[i] = 0
+    else: loc1allele2[i] = 1
+    loc2allele1[i] = 0
+    loc2allele2[i] = 0
+    loc3allele1[i] = 0
+    loc3allele2[i] = 0
 
 # start main loop
-for i in range(1, NumGens+1):
+for i in range(1, numgens+1):
     pass
